26958d9e44cef283881cdc1f742a2a7c149fc086
Chiranth D <chiranthdbca25@rvu.edu.in>
2026-02-27 12:05:58 +0530

    Add files via upload

---
+++ lab3.sh
@@ -0,0 +1,22 @@
+echo "job 1:Display  Date"
+date
+sleep 3
+
+echo "job 2:Display current user"
+whoami
+sleep 3
+
+echo "job 3:Display Calendar"
+cal
+sleep 2
+
+echo "job 4:List in files"
+ls
+sleep 3
+
+echo "job 5:Display my system date"
+date +%T
+sleep 1
+
+echo "job 6"
+pwd 
